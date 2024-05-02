93e9072d529d0644d699bb215ff20e161c723409
Taz Uddin <tazuddin@wafisolutions.com>
2024-05-02 14:38:40 +0600

    Try aspose

---
+++ ImageResizer/AsposeImageResizer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageResizer
+{
+    public class AsposeImageResizer
+    {
+        public static void Resize(string dir)
+        {
+
+            using (Aspose.Imaging.Image image = Aspose.Imaging.Image.Load(dir + "T2_test.png"))
+            {
+                // Scale up by 2 times using Nearest Neighbour resampling.
+                image.Resize(image.Width * 2, image.Height * 2, Aspose.Imaging.ResizeType.NearestNeighbourResample);
+                image.Save(dir + "upsample.nearestneighbour.png");
+            }
+
+            using (Aspose.Imaging.Image image = Aspose.Imaging.Image.Load(dir + "T2_test.png"))
+            {
+                // Scale down by 2 times using Nearest Neighbour resampling.
+                image.Resize(image.Width / 2, image.Height / 2, Aspose.Imaging.ResizeType.NearestNeighbourResample);
+                image.Save(dir + "downsample.nearestneighbour.png");
+            }
+
+            using (Aspose.Imaging.Image image = Aspose.Imaging.Image.Load(dir + "T2_test.png"))
+            {
+                // Scale up by 2 times using Bilinear resampling.
+                image.Resize(image.Width * 2, image.Height * 2, Aspose.Imaging.ResizeType.BilinearResample);
+                image.Save(dir + "upsample.bilinear.png");
+            }
+
+            using (Aspose.Imaging.Image image = Aspose.Imaging.Image.Load(dir + "T2_test.png"))
+            {
+                // Scale down by 2 times using Bilinear resampling.
+                image.Resize(image.Width / 2, image.Height / 2, Aspose.Imaging.ResizeType.BilinearResample);
+                image.Save(dir + "downsample.bilinear.png");
+            }
+        }
+    }
+}
--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -5,28 +5,33 @@ namespace ImageResizer;
 
 public static class Program
 {
-    private static string InputPath = "C:\\Users\\Taz\\Downloads\\ImageResizer\\ImageResizer\\ImageResizer\\Input\\panda.jpg";
-    private static string OutputPath = "C:\\Users\\Taz\\Downloads\\ImageResizer\\ImageResizer\\ImageResizer\\Output";
+    private static string InputPath = "C:\\work\\lab\\image-resizer-c-sharp\\ImageResizer\\Input\\wood_Black.png";
+    private static string OutputPath = "C:\\work\\lab\\image-resizer-c-sharp\\ImageResizer\\Output";
     public static void Main(string[] args)
     {
 
 
+        AsposeImageResizer.Resize("C:\\work\\lab\\image-resizer-c-sharp\\ImageResizer\\Input\\");
+        
+        Console.WriteLine("Hello world");
+    }
+
+    public static void UseNative()
+    {
         var image = Image.FromFile(InputPath);
 
         using (var ms = new MemoryStream())
         {
             image.Save(ms, ImageFormat.Jpeg);
 
-            var b = Utility.ZoomImage(ms.ToArray(), 0.02f);
+            var b = Utility.ZoomImage(ms.ToArray(), 0.2f);
 
             using (var js = new MemoryStream(b))
             {
                 var resized = Image.FromStream(js);
-                
-                resized.Save(Path.Combine(OutputPath, "resized.jpeg"));
+
+                resized.Save(Path.Combine(OutputPath, "wood_Black.png"));
             }
         }
-        
-        Console.WriteLine("Hello world");
     }
 }
